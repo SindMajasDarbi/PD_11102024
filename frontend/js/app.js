86a592393222c73bf7e37f0f79e02b6b96305069
unknown <your@email.example>
2024-10-11 10:27:43 +0300

    T_T

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -4,14 +4,14 @@ window.onload = function() {
         event.preventDefault();
 
         let formData = new FormData(event.target);
-        let token = formData.get('token');
+        globalToken = formData.get('token');
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/user', {
+            const response = await fetch('http://127.0.0.1:8000/api/user', {
                 method: 'GET',
                 headers: {
-                    'Authorization': `Bearer ${token}`,
-                    'Content-Type': 'application/html'
+                    'Authorization': `Bearer ${globalToken}`,
+                    'Content-Type': 'application/json'
                 }
             });
 
@@ -36,7 +36,7 @@ window.onload = function() {
         let token = formData.get('token');
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/posts', {
+            const response = await fetch('http://127.0.0.1:8000/api/posts', {
                 method: 'POST',
                 headers: {
                     'Authorization': `Bearer ${token}`,
@@ -44,7 +44,7 @@ window.onload = function() {
                 },
                 body: JSON.stringify({
                     title: formData.get('title'),
-                    body: formData.get('title')
+                    body: formData.get('body')
                 })
             });
 
@@ -66,7 +66,7 @@ window.onload = function() {
 
     async function fetchAllPosts(token) {
         try {
-            const response = await fetch('http://127.0.0.1:8000/posts', {
+            const response = await fetch('http://127.0.0.1:8000/api/posts', {
                 method: 'POST',
                 headers: {
                     'Authorization': `Bearer ${token}`,
